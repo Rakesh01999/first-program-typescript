52181723f65c038d4c83a85841c7d878e80052c7
Rakesh Biswas <rbiswas01999@gmail.com>
2024-09-20 20:41:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var userName = "Rakesh Biswas";
+// userName = 234;
+var age = 30;
+console.log(userName);
+console.log(age);
---
+++ index.ts
@@ -0,0 +1,5 @@
+let userName = "Rakesh Biswas";
+// userName = 234;
+let age = 30;
+console.log(userName);
+console.log(age);
