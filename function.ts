39c4767da8b8522e0f0cfe8510de855744c5e963
Rakesh Biswas <rbiswas01999@gmail.com>
2024-09-20 20:51:53 +0600

    new code added

--- function.ts
+++ function.ts
@@ -2,4 +2,7 @@ function addNumbers(num1, num2)  {
     return (num1 + num2);
 }
 
-console.log(addNumbers(12, 14));
+// console.log(addNumbers(12, 14));
+console.log(addNumbers(12, "14")); // the second one is not number 
+
+
