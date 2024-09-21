a3c29d821de83eb1dba8e873c6e580a63d9ac5e1
Rakesh Biswas <rbiswas01999@gmail.com>
2024-09-21 12:56:43 +0600

    tuple-type added

---
+++ user-defined-type/tuple-type.js
@@ -0,0 +1,7 @@
+// mixed data
+var user;
+user = ['Kashem', 34];
+user.push("Zisan", 234);
+user.push(37, "halim");
+console.log(user);
+console.log(user[0]);
---
+++ user-defined-type/tuple-type.ts
@@ -0,0 +1,8 @@
+// mixed data
+let user: (string | number)[];
+user = ['Kashem', 34];
+
+user.push("Zisan", 234);
+user.push( 37, "halim");
+console.log(user);
+console.log(user[0]);
